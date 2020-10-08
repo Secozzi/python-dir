c37357440cbaee0fa68409d826665cd1d2d62336
Secozzi <49240133+Secozzi@users.noreply.github.com>
2020-10-08 06:31:57 +0200

    Reserved name to PyPi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='python-dir',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/Secozzi/python-dir',
+    author='Folke Ishii',
+    author_email='folke.ishii@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
